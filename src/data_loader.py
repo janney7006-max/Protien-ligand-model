af5a2adfd5bb3e33386d24dc755c1991f420f605
janney7006-max <janney7006@gmail.com>
2026-06-24 18:55:40 +0530

    Add data loader module

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,15 @@
+from Bio.PDB import PDBParser
 
+class ProteinLoader:
+    def __init__(self):
+        self.parser = PDBParser(QUIET=True)
+    
+    def load_pdb(self, pdb_file):
+        """Load PDB protein structure file"""
+        structure = self.parser.get_structure('protein', pdb_file)
+        return structure
+    
+    def get_atoms(self, structure):
+        """Get all atoms from protein"""
+        atoms = list(structure.get_atoms())
+        return atoms
